f30af06a0eeeb4d04a2582481d4e8042238359ef
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-25 11:19:32 -0500

    playlist service function/ basic track display

--- src/app/play-list-service.service.ts
+++ src/app/play-list-service.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlayListServiceService {
-
+  playlist1: Object[];
+  playlist2: Object[]
   constructor() { }
+  addToPlaylist1(song){
+    this.playlist1.push(song)
+  }
+  addToPlaylist2(song){
+    this.playlist2.push(song)
+  }
 }
