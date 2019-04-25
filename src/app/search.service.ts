d90f5fed81f0918f3d1a18db37bc252aff6b9038
Ian Gurney <ianclaytongurney@gmail.com>
2019-04-25 14:42:11 -0500

    Update search.service.ts

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TrackContainer } from './models/track.model';
 import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
+  trackList: TrackContainer[];
+
   constructor(private http: HttpClient) { }
 
   getSearchResults(searchQuery: string){
-    return this.http.get(`http://api.musixmatch.com/ws/1.1/track.search?q=${searchQuery}&apikey=7d2a2a73f5654da26590892785c1ff47`).pipe(map((res: Object) => res['message'].body.tracklist))
+    this.http.get(`http://api.musixmatch.com/ws/1.1/track.search?q=${searchQuery}&apikey=7d2a2a73f5654da26590892785c1ff47`).pipe(map((res: Object) => res['message'].body.tracklist)).subscribe(res => this.trackList = res);
+  }
+
+  getTracks(){
+    return of(this.trackList);
   }
 }
+ 
