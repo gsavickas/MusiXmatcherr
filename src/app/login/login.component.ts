84a85cbf45b5ed083140f01c639e9659b40e6382
Paul Krapf <paulkrapf@gmail.com>
2019-04-26 17:09:05 -0500

    updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from 'protractor';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  User: Object={
+    userID: '',
+    password: '',
+    button(click): ''
+  }
   constructor() { }
 
   ngOnInit() {
