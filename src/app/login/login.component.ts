818ab47bfd6e1f29fe7b4c01b8fc014aa5a34dba
Paul Krapf <paulkrapf@gmail.com>
2019-04-28 14:07:26 -0500

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Button } from 'protractor';
 import { UserServiceService } from '../user-service.service';
 import { FormsModule } from '@angular/forms';
-import { ApiService } from '../api.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,25 +15,9 @@ export class LoginPageComponent implements OnInit {
     password: 'test'
   }
   
-  constructor(private api: ApiService, private router: Router) {
+  constructor(private user: User, private router: Router) {
   }
 
-  tryLogin() {
-    this.api.login(
-      this.userID,
-      this.password
-    )
-      .subscribe(
-        r => {
-          if (r.token) {
-            this.customer.setToken(r.token);
-            this.router.navigateByUrl('/dashboard');
-          }
-        },
-        r => {
-          alert(r.error.error);
-        });
-  }
   ngOnInit() {
   }
 
