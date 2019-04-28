85f1b7e32c5b4687f34f7e9e0f07284ca77de91f
Paul Krapf <paulkrapf@gmail.com>
2019-04-28 12:24:36 -0500

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,19 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { Button } from 'protractor';
 import { UserServiceService } from '../user-service.service';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginPageComponent implements OnInit {
   User: Object={
-    userID: '',
-    password: ''
+    userID: 'test',
+    password: 'test'
   }
-  constructor() { }
+  
+  constructor(private api: ApiService, private router: Router) {
+  }
 
+  tryLogin() {
+    this.api.login(
+      this.userID,
+      this.password
+    )
+      .subscribe(
+        r => {
+          if (r.token) {
+            this.customer.setToken(r.token);
+            this.router.navigateByUrl('/dashboard');
+          }
+        },
+        r => {
+          alert(r.error.error);
+        });
+  }
   ngOnInit() {
   }
 
